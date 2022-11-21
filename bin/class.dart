61bda078bd8d05f1bb1ab24abd4037466785d23e
sharonrmichael <sharonrmichaelqp@gmail.com>
2022-11-21 16:00:27 +0530

    commit

---
+++ bin/class.dart
@@ -0,0 +1,25 @@
+
+class Resume{
+
+  String name = "SHARK";
+  int     age = 21;
+  int   phone = 515559206;
+  double mark = 5.5;
+  String email= "sharonrmicaelqq@gmail.com";
+
+}
+
+void main(){
+
+  Resume stu1 = Resume();
+  print("Student 1 Resume");
+  print("Name = ${stu1.name}");
+  print("Age = ${stu1.age}");
+  print("Phone = ${stu1.phone}");
+
+  Resume stu2 = Resume();
+  print("Student 2 Resume");
+  print('Name = ${stu2.name ='pp chithembaram' }');
+  print('Age = ${stu2.age = 20 }');
+  print('Phone = ${stu2.phone =1654626552}');
+}
