b8586bec4a1a4cb84770978d1ef935c3b37ce6a3
sharonrmichael <sharonrmichaelqp@gmail.com>
2022-11-21 15:14:41 +0530

    commit

---
+++ bin/variables.dart
@@ -0,0 +1,15 @@
+void main(){
+
+  String name = "SHARK";
+  int     age = 21;
+  int   phone = 515559206;
+  double mark = 5.5;
+  String email= "sharonrmicaelqq@gmail.com";
+
+  print("---hhhshs---");
+  print("my name is $name");
+  print("i am $age years old");
+  print("my phn number is $phone");
+  print("my mark is $mark");
+  print("my email id is $email");
+}
